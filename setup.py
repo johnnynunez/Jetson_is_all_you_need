f457f0554f05ef58d8d691e1ecf5e299f0fd1df4
johnnynunez <johnnynuca14@gmail.com>
2023-11-22 10:39:23 +0100

    .

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="Compile automatically Jetson Packages",
+    author="Johnny Núñez Cano",
+    author_email="jnunezca11@alumnes.ub.edu",
+    url="https://github.com/johnnynunez/Jetson_is_all_you_need",
+    install_requires=[""],
+    packages=find_packages(),
+)
